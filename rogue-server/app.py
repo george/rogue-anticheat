635da9a915251f3501a1d5f36a99e530e6bb862d
Gardening_Tool <sam.polshek@gmail.com>
2022-08-21 00:07:29 -0500

    Added route for check handling

--- rogue-server/app.py
+++ rogue-server/app.py
@@ -1,8 +1,31 @@
 from flask import Flask
 
+from flask import request
+from flask import Response
+
 import config
+import json
 
 app = Flask(__name__)
+
+
+@app.route('/players/<id>/', methods=['POST'])
+def handle_players_route(id):
+    data = request.get_json()
+    print(id)
+    print(data)
+
+    return Response(json.dumps({
+        'violations': {
+            'a': {
+                'checkName': 'Test',
+                'checkType': 'A',
+                'violations': 1,
+                'maxViolations': 2
+            }
+        }
+    }), status=200)
+
 
 if __name__ == "__main__":
     app.run(config.hostname, config.port)
