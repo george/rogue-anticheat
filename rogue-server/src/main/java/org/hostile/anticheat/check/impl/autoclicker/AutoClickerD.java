56ca1a078d942a303776cfd9247529dc0626b5e6
George <sam.polshek@gmail.com>
2022-12-02 22:12:17 -0600

    Fixed AutoClicker D false flagging

--- rogue-server/src/main/java/org/hostile/anticheat/check/impl/autoclicker/AutoClickerD.java
+++ rogue-server/src/main/java/org/hostile/anticheat/check/impl/autoclicker/AutoClickerD.java
@@ -7,6 +7,9 @@ import org.hostile.anticheat.event.PacketEvent;
 import org.hostile.anticheat.packet.inbound.WrappedPacketPlayInArmAnimation;
 import org.hostile.anticheat.packet.inbound.WrappedPacketPlayInFlying;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -16,9 +19,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 @CheckMetadata(type = "AutoClicker", name = "D")
 public class AutoClickerD extends PacketCheck {
 
-    private final Queue<Integer> ticks = new ConcurrentLinkedQueue<>();
-
-    private long lastCheck;
+    private final List<Integer> ticks = Collections.synchronizedList(new ArrayList<>());
 
     public AutoClickerD(PlayerData playerData) {
         super(playerData);
@@ -26,29 +27,27 @@ public class AutoClickerD extends PacketCheck {
 
     @Override
     public void handle(PacketEvent event) {
-        if (event.getPacket() instanceof WrappedPacketPlayInFlying) {
-            if (event.getTimestamp() - lastCheck > 1000) {
-                int cps = this.ticks.size();
+        if (event.getPacket() instanceof WrappedPacketPlayInFlying && data.getTicksExisted() % 20 == 0) {
+            int cps = this.ticks.size();
 
-                if (cps > 15) {
-                    double distinct = this.ticks.stream().distinct().count();
-                    double doubleClicks = cps - distinct;
-                    double ratio = distinct / doubleClicks;
+            if (cps >= 16) {
+                double distinct = this.ticks.stream().distinct().count();
+                double doubleClicks = cps - distinct;
+                double ratio = distinct / doubleClicks;
 
-                    if (doubleClicks < 1) {
-                        if (incrementBuffer(1) > 3) {
-                            fail("cps", cps, "distinct", distinct, "doubleClicks", doubleClicks, "ratio", ratio);
-                        }
-                    } else {
-                        decrementBuffer(0.5);
+                if (doubleClicks < 1) {
+                    if (incrementBuffer(1) > 3) {
+                        fail("cps", cps, "distinct", distinct, "doubleClicks", doubleClicks, "ratio", ratio);
                     }
-
-                    this.ticks.clear();
-                    this.lastCheck = event.getTimestamp();
+                } else {
+                    decrementBuffer(0.5);
                 }
             }
+
+            this.ticks.clear();
         } else if (event.getPacket() instanceof WrappedPacketPlayInArmAnimation && !actionTracker.isDigging()) {
             this.ticks.add(data.getTicksExisted());
+            System.out.println(data.getTicksExisted());
         }
     }
 }
